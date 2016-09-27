4fc0780cdcaa1c6457b91fa747ec21209f9b1983
Vladimir Morulus <vladimirmorulus@gmail.com>
2016-09-27 19:46:49 +0300

    fix path version generator

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 
 function increaseMinorVersion() {
   var packageJson = this.touch('package.json').require();
-  packageJson.version = packageJson.version.split('.').map(function(v, index) { return index===1 ? parseInt(v)+1 : v; }).join('.');
+  packageJson.version = packageJson.version.split('.').map(function(v, index) { return index===1 ? parseInt(v)+1 : (index===2 ? 0 : v); }).join('.');
   fs.writeFileSync(require.resolve('./package.json'), JSON.stringify(packageJson, null, 2), 'utf-8');
   return packageJson;
 }
