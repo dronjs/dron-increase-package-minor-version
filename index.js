e84273be60fb912e4547d007e8335bfb4714ab6d
Vladimir Morulus <vladimirmorulus@gmail.com>
2016-09-27 05:23:42 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+var fs = require('fs');
+
+function increaseMinorVersion() {
+  var packageJson = this.touch('package.json').require();
+  packageJson.version = packageJson.version.split('.').map(function(v, index) { return index===1 ? parseInt(v)+1 : v; }).join('.');
+  fs.writeFileSync(require.resolve('./package.json'), JSON.stringify(packageJson, null, 2), 'utf-8');
+  return packageJson;
+}
+
+module.exports = function increaseMinorVersionFactoy() {
+  return function() {
+    return this.run(increaseMinorVersion)
+    .then(function(info) {
+      return this.run('gitcommit', {
+        message: 'Minor release '+info.version
+      });
+    }.bind(this));
+  }
+}
